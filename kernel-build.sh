9dfe1e8e439d8b3c01337691a385175f08a61571
kikomachine <jfdsmabalot@gmail.com>
2014-08-02 02:50:29 +0000

    scripts: point to default config Signed-off-by: Jean Francis Dominique S. Mabalot <jmabalot@google.com>

--- kernel-build.sh
+++ kernel-build.sh
@@ -18,5 +18,5 @@ make mrproper
 export ARCH=arm
 export CROSS_COMPILE=~/tmp/arm-eabi-4.9/bin/arm-eabi-
 export ENABLE_GRAPHITE=true
-make hammerhead_defconfig
+make cyanogenmod_bacon_defconfig
 time make -j8 2>&1 | tee kernel.log
